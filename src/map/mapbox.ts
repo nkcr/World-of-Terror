0309562d7406f12bb5b0b33dbd7a286eb92c9896
matthias.christe <matthias.christe@edu.hefr.ch>
2017-12-14 15:16:13 +0100

    icons changed

--- src/map/mapbox.ts
+++ src/map/mapbox.ts
@@ -33,11 +33,11 @@ export default class Mapbox {
         if (db[i][1] >= dstart && db[i][1] <= dend) {
            var attackType =db[i][29];
            var latlng = new L.LatLng(db[i][13], db[i][14]);
-
-           var attack_icon_url = "assets/images/icons/" + db[i][28] + ".svg";
+           var success = db[i][26];
+           var attack_icon_url = "assets/images/icons/" + db[i][28] + "_" + success + ".png";
            var myIcon = L.icon({
               iconUrl: attack_icon_url,
-              iconSize:     [38, 95], // size of the icon
+              iconSize:     [50, 50], // size of the icon
            });
            var marker = L.marker(latlng, {
               icon: myIcon,
@@ -45,7 +45,7 @@ export default class Mapbox {
            });
 
 
-           var popup = this.createPopup(db, i, attack_icon_url, latlng)
+           var popup = this.createPopup(db, i, attackType, attack_icon_url, latlng, success)
            marker.bindPopup(popup);
 
 
@@ -74,20 +74,26 @@ export default class Mapbox {
     this.mapbox.addLayer(this.markers);
   }
 
-  createPopup(db :Array<any>, i:number,  attack_icon_url:string, latlng:any){
+  createPopup(db :Array<any>, i:number,  attackType:string, attack_icon_url:string, latlng:any, success:any){
      let summary:string = db[i][18];
 
      if(!summary){
        summary = 'Sorry, no summary is available.';
      }
+     let success_url =  "assets/images/success/" + success + ".png";
      let dateOfAttack:string = db[i][1] + '/' + db[i][2] + '/' + db[i][3];
      let gname:string = db[i][58];
      let targetType:string = db[i][35];
-     let icon_img = '<img src=' + attack_icon_url+ ' height=25 width=25/>';
-     let content =   '<h1>' + dateOfAttack + '</h1>' +
-                     '<table>' +
-                     '<tr><th>Perpetrator</th><th>Attack type</th><th>Target</th></tr>'+
-                     '<tr><td>' + gname +'</td><td>' + icon_img +'</td><td>' + targetType + '</td></tr>'+
+     let icon_img = '<img src=' + attack_icon_url  + ' height=35 width=35/>';
+     let success_img = '<img src=' + success_url +  ' height=25 width=25/>';
+     let content =   '<h2>' + icon_img + ' ' + attackType + '</h2>' +
+                     /*'<table>' +
+                     '<tr><th>Perpetrator</th><th>Success</th><th>Target</th></tr>'+
+                     '<tr><td>' + gname +'</td><td>' + success_img +'</td><td>' + targetType + '</td></tr>'+
+                     '</table>' +*/
+                     '<table align=center>' +
+                     '<tr><td align=center>' + dateOfAttack +'</td><td align=center>' + '<strong>Succes </strong>' + success_img +'</td></tr>'+
+                     '<tr><td align=center>' + '<strong>Perpetrator</strong><br>' + gname +'</td><td align=center>' + '<strong>Target</strong><br>' + targetType +'</td></tr>'+
                      '</table>' +
                      '<hr>' +
                      '<p>' + summary  + '</p>';
